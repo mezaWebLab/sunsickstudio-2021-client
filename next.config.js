f2400c70b3fbbf981f4ada0790290ada4ad09286
ameza <ameza@rescueagency.com>
2021-07-26 01:38:34 -0700

    added redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     env: {
       SUNSICK_API_URL: process.env.SUNSICK_API_URL,
+    },
+    async redirects() {
+      return [
+        {
+          source: '/admin',
+          destination: 'https://api.sunsickstudio.mezaweblab.com/admin',
+          permanent: true
+        }
+      ]
     }
 }
